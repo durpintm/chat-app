3afcfb460654956ddf6ffd3d102d182901f258f4
durpintm <durpinthapa@gmail.com>
2024-06-26 23:28:41 -0400

    check if email exists in db

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const registerUser = require("../controller/registerUser.js");
+const checkEmail = require("../controller/checkEmail.js");
 
 const router = express.Router();
 
 // create user api
 router.post("/register", registerUser);
+
+// check user email
+router.post("/email", checkEmail);
 
 module.exports = router;
