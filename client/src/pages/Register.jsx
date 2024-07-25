ef4734d671c73fee3b393e1d410f5c659ca40010
durpintm <durpinthapa@gmail.com>
2024-07-25 01:34:25 -0400

    Register page added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,22 +1,124 @@
+import { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profile_pic: "",
+  });
+
+  const [uploadPhoto, setUploadPhoto] = useState("");
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleUploadPhoto = (e) => {
+    const file = e.target.files[0];
+    setUploadPhoto(file);
+  };
+
+  const handleClearUploadPhoto = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setUploadPhoto(null);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(data);
+  };
+
   return (
     <div className="mt-5">
-      <div className="bg-white w-full max-w-md mx-2 rounded overflow-hidden p-4">
+      <div className="bg-white w-full max-w-lg rounded overflow-hidden p-4 mx-auto">
         <h3 className="font-bold text-2xl text-primary">
           Welcome to Chat App!
         </h3>
-        <form action="">
-          <div>
+        <form className="grid gap-4 mt-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-2">
             <label htmlFor="name">Name:</label>
             <input
               type="text"
               id="name"
               name="name"
+              value={data.name}
+              onChange={handleOnChange}
               placeholder="Enter your name"
               className="bg-slate-100 px-2 py-1 focus:outline-primary"
             />
           </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={data.email}
+              onChange={handleOnChange}
+              placeholder="Enter your email"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={data.password}
+              onChange={handleOnChange}
+              placeholder="Enter your password"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="profile_pic">
+              Photo:
+              <div className="h-14 bg-slate-200 flex justify-center items-center border hover:border-primary rounded cursor-pointer">
+                <p className="text-sm max-w-[300px] text-ellipsis line-clamp-1">
+                  {uploadPhoto?.name
+                    ? uploadPhoto?.name
+                    : "Upload profile photo"}
+                </p>
+                {uploadPhoto?.name && (
+                  <button
+                    className="text-lg ml-2 hover:text-red-600"
+                    onClick={handleClearUploadPhoto}
+                  >
+                    <IoCloseSharp />
+                  </button>
+                )}
+              </div>
+            </label>
+
+            <input
+              type="file"
+              id="profile_pic"
+              name="profile_pic"
+              value={data.profile_pic}
+              className="bg-slate-100 px-2 py-1 focus:outline-primary hidden"
+              onChange={handleUploadPhoto}
+            />
+          </div>
+          <button className="bg-primary text-lg px-3 py-2 hover:bg-secondary rounded mt-4 font-bold text-white leading-relaxed tracking-wider">
+            Register
+          </button>
         </form>
+        <p className="my-4 text-center">
+          Already have account?
+          <Link to={"/email"} className="hover:text-primary font-semibold">
+            {" "}
+            Login
+          </Link>
+        </p>
       </div>
     </div>
   );
