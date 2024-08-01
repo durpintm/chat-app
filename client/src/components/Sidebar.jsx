7e518b62a749896ac3332f2feaadf6566b2ab087
durpintm <durpinthapa@gmail.com>
2024-07-31 23:15:35 -0400

    Message section added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,13 +6,16 @@ import Avatar from "./Avatar";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import EditUserDetails from "./EditUserDetails";
+import { FiArrowUpLeft } from "react-icons/fi";
 
 const Sidebar = () => {
   const user = useSelector((state) => state.user);
   const [editUserOpen, setEditUserOpen] = useState(false);
+  const [allUser, setAllUser] = useState([]);
+  const [openSerchUser, setOpenSearchUser] = useState(false);
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full grid grid-cols-[48px,1fr] bg-white">
       <div className="bg-slate-100 w-12 h-full rounded-tr-md rounded-br-md py-5 text-slate-700 flex flex-col justify-between">
         <div>
           <NavLink
@@ -59,9 +62,31 @@ const Sidebar = () => {
         </div>
       </div>
 
+      <div className="w-full">
+        <div className="h-16 flex items-center">
+          <h2 className="text-xl font-bold p-4 text-slate-800">Messages</h2>
+        </div>
+        <div className="bg-slate-200 p-[0.5px]"></div>
+        <div className="h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar">
+          {allUser.length === 0 && (
+            <div className="mt-12">
+              <div className="flex justify-center items-center my-4 text-slate-500">
+                <FiArrowUpLeft size={50} />
+              </div>
+              <p className="text-lg text-center text-slate-400">
+                Explore Users to Start a Conversation!
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+
       {editUserOpen && (
         <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
       )}
+
+      {/* Search Users */}
+      {openSerchUser}
     </div>
   );
 };
