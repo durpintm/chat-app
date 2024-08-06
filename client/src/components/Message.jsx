740b2a4dce8cc6c4d5a126aedef2095e96c6e5ef
durpintm <durpinthapa@gmail.com>
2024-08-05 23:44:45 -0400

    Show user info in the header

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Avatar from "../components/Avatar";
+import { HiDotsVertical } from "react-icons/hi";
+import { FaAngleLeft } from "react-icons/fa6";
 
 const Message = () => {
   const params = useParams();
   const socketConnection = useSelector(
     (state) => state?.user?.socketConnection
   );
+  const user = useSelector((state) => state?.user);
+
   const [userData, setUserData] = useState({
     name: "",
     email: "",
@@ -24,12 +28,15 @@ const Message = () => {
         setUserData(data);
       });
     }
-  }, [socketConnection, params?.userId]);
+  }, [socketConnection, params?.userId, user]);
 
   return (
     <div>
-      <header className="sticky top-0 h-16 bg-white">
-        <div>
+      <header className="sticky top-0 h-16 bg-white flex justify-between items-center px-4">
+        <div className="flex items-center gap-4">
+          <Link to={"/"} className="lg:hidden">
+            <FaAngleLeft size={20} />
+          </Link>
           <div>
             <Avatar
               width={50}
@@ -39,6 +46,26 @@ const Message = () => {
               userId={userData._id}
             />
           </div>
+          <div>
+            <h3 className="font-semibold text-md my-0 text-ellipsis line-clamp-1">
+              {userData?.name}
+            </h3>
+            <p className="-my-2">
+              {userData.online ? (
+                <span className="text-primary font-bold text-sm">online</span>
+              ) : (
+                <span className="text-slate-500 font-bold text-sm">
+                  offline
+                </span>
+              )}
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <button className="cursor-pointer hover:text-primary">
+            <HiDotsVertical />
+          </button>
         </div>
       </header>
     </div>
